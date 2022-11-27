853c15db3cc8771d71a33c69e91b81df4647faf4
RotenKiwi <joshiparth212@gmail.com>
2022-11-27 13:36:35 +0530

    design phase
    bottom blue aligned, bottom orange not alligned

--- lib/screens/splashscreen.dart
+++ lib/screens/splashscreen.dart
@@ -12,15 +12,32 @@ class _splashscreenState extends State<splashscreen> {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Container(
-      color: Color(0xffffffff),
-      height: size.height,
-      width: double.infinity,
-      child: Container(decoration: BoxDecoration(
-          image: DecorationImage(
-              fit: BoxFit.cover,
-              image: AssetImage('assets/images/background.png')
-          )
-      ),),
-    );
+        color: Color(0xffffffff),
+        height: size.height,
+        width: double.infinity,
+        child: Stack(alignment: Alignment.bottomCenter, children: <Widget>[
+          Positioned(
+            child: Container(
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      fit: BoxFit.cover,
+                      image: AssetImage('assets/images/background.png'))),
+            ),
+          ),
+
+          Positioned(
+              child: Container(
+            width: double.infinity,
+            height: size.height * 0.035,
+            decoration: BoxDecoration(color: Color(0xff2b2c7f)),
+          )),
+          Positioned(
+              bottom: size.height * 0.035,
+              child: Container(
+                width: double.infinity,
+                height: size.height * 0.035 * 0.5,
+                decoration: BoxDecoration(color: Color(0xfffd7105)),
+              ))
+        ]));
   }
 }
