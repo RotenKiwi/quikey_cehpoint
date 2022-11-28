526082cbfde8edd2e3cd9a759014525ce7a8644e
RotenKiwi <joshiparth212@gmail.com>
2022-11-29 02:08:36 +0530

    pre-work
    commit before starting to work again

--- lib/screens/LoginPage.dart
+++ lib/screens/LoginPage.dart
@@ -14,27 +14,38 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return Container(
-      color: Color(0xffffffff),
-      child: background(
-        size: size,
-        child: Stack(
-          alignment: Alignment.topCenter,
-          children: <Widget>[
-            Positioned(
-                top: 0,
-                child: Image.asset(
-                  'assets/images/Login.png',
-                  width: size.width * 0.8,
-                )),
-            Positioned(
-                top: size.height * 0.348,
-                child: DualButton(size: size, mode: 'login',))
-          ],
+    return Scaffold(
+      body: Container(
+        color: Color(0xffffffff),
+        child: background(
+          size: size,
+          child: Stack(
+            alignment: Alignment.topCenter,
+            children: <Widget>[
+              Positioned(
+                  top: 0,
+                  child: Image.asset(
+                    'assets/images/Login.png',
+                    width: size.width * 0.8,
+                  )),
+              Positioned(
+                  top: size.height * 0.348,
+                  child: DualButton(
+                    size: size,
+                    mode: 'login',
+                  )),
+              Positioned(
+                  top: size.height * 0.3,
+                  child: Padding(
+                    padding: EdgeInsets.all(0.3),
+                    child: TextField(
+                      decoration: InputDecoration(labelText: 'Email'),
+                    ),
+                  ))
+            ],
+          ),
         ),
       ),
     );
   }
 }
-
-
