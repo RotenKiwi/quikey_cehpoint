de926cbe800b71c8ecc1555fe037f9b7ceafbf47
RotenKiwi <joshiparth212@gmail.com>
2022-11-29 19:16:10 +0530

    profile setup
    added blank pfp

--- lib/components/SignupDetails.dart
+++ lib/components/SignupDetails.dart
@@ -19,15 +19,15 @@ class _SignUpDetailsState extends State<SignUpDetails> {
     Size size = MediaQuery.of(context).size;
     return Scaffold(
         body: Container(
-      color: Color(0xffd9d9d9),
+      color: Color(0xffffffff),
       child: background_nobottom(
           size: size,
           child: Stack(
             alignment: Alignment.topCenter,
             children: <Widget>[
               Positioned(
-                  top: size.height * 0.08,
-                  left: size.width * 0.06,
+                  top: size.height * 0.06,
+                  left: size.width * 0.05,
                   child: backbutton(
                     press: () {
                       Navigator.push(
@@ -37,13 +37,32 @@ class _SignUpDetailsState extends State<SignUpDetails> {
                     },
                   )),
               Positioned(
-                  top: size.height * 0.07,
+                  top: size.height * 0.053,
                   child: RoundedField(
                     text: 'PROFILE SETUP',
                     color: Color(0xff2b2c7f),
                     length: size * 0.36,
-                    textColor: Color(0xffffffff), tall: size*0.06,
-                  ))
+                    textColor: Color(0xffffffff),
+                    tall: size * 0.06,
+                  )),
+              Positioned(
+                  top: size.height * 0.153,
+                  child: Text(
+                    'You Are Just One Step Away From Your\nSocial Media Journey',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        fontWeight: FontWeight.w400,
+                        fontFamily: 'Poppins',
+                        fontSize: size.width * 0.047,
+                        color: Color(0xff606060)),
+                  )),
+              Positioned(
+                  top: size.height*0.244,
+                  child: CircleAvatar(
+                    backgroundColor: Color(0xffd9d9d9),
+                    radius: size.width*0.176,
+                    child: Icon(Icons.person_outlined, size: size.height*0.15, color: Color(0xff606060),),)),
+
             ],
           )),
     ));
